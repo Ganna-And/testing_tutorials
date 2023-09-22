56a3bbfcd88869f4316a0ca7b03e0c928e253977
= <ganna.andrushko@gmail.com>
2023-09-22 16:06:58 +0800

    describe method to nest test blocks

--- testing/src/components/greed/greed.test.tsx
+++ testing/src/components/greed/greed.test.tsx
@@ -1,15 +1,19 @@
 import { render, screen } from "@testing-library/react"
 import Greed from "./greed"
 
-test('greet is rendering correctly',()=>{
+describe('Greet', ()=>{
+   test('is rendering correctly',()=>{
     render(<Greed />);
     const textElement = screen.getByText('Hello')
     expect(textElement).toBeInTheDocument()
-});
+})
 
-test('greet renders with the name', ()=>{
+})
+describe('Nested', ()=>{
+    test('renders with the name', ()=>{
     render(<Greed name='Vishman' />)
     const textElement = screen.getByText("Hello Vishman")
     expect(textElement).toBeInTheDocument()
     
+}) 
 })
